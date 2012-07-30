8e0a3b7daf472cf308306efdba09b4fa561fbda9
Gord Tanner <gtanner@gmail.com>
2012-07-30 16:49:54 -0400

    added blackberry.media.camera support to webworks.handset, quick stub in for allowing capture of video

---
+++ test/unit/webworks/camera.js
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2011 Research In Motion Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+describe("webworks camera", function () {
+    var camera = require('ripple/platform/webworks.core/2.0.0/client/camera'),
+        event = require('ripple/event'),
+        ui = require('ripple/ui/plugins/camera');
+
+    describe("when closing", function () {
+        it("hides the ui camera", function () {
+            spyOn(ui, "hide");
+
+            camera.close();
+            expect(ui.hide).toHaveBeenCalled();
+        });
+    });
+
+    describe("when taking a picture", function () {
+        var success = jasmine.createSpy(),
+            error = jasmine.createSpy(),
+            close = jasmine.createSpy();
+
+        beforeEach(function () {
+            spyOn(ui, "show");
+            spyOn(event, "once");
+        });
+
+        it("shows the ui camera", function () {
+            camera.takePicture();
+            expect(ui.show).toHaveBeenCalled();
+        });
+
+        it("registers for the captured-image event", function () {
+            camera.takePicture(success);
+            expect(event.once).toHaveBeenCalledWith("captured-image", jasmine.any(Function));
+        });
+
+        it("calls the success callback with a path to the file from the captured-image event", function () {
+            camera.takePicture(success);
+            event.once.mostRecentCall.args[1]("file://pics/duck_face.jpg");
+            expect(success).toHaveBeenCalledWith("file://pics/duck_face.jpg");
+        });
+
+        it("registers for the camera-closed event with the onCameraClosed callback", function () {
+            camera.takePicture(success, close);
+            expect(event.once).toHaveBeenCalledWith("camera-closed", close);
+            
+        });
+    });
+});
